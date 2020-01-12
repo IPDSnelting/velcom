6d5c13d0eca9f4e4cf188df25930f8405caff0df
kwerber <werberkevin@gmail.com>
2020-01-12 19:35:00 +0100

    Implement TokenAccess

--- backend/backend/src/main/java/de/aaaaaaah/velcom/backend/access/token/HashAlgorithm.java
+++ backend/backend/src/main/java/de/aaaaaaah/velcom/backend/access/token/HashAlgorithm.java
@@ -1,6 +1,8 @@
 package de.aaaaaaah.velcom.backend.access.token;
 
-// TODO When exactly do we need this class?
+import de.mkammerer.argon2.Argon2;
+import de.mkammerer.argon2.Argon2Factory;
+import de.mkammerer.argon2.Argon2Helper;
 
 /**
  * Represents a certain hashing algorithm used for storing tokens.
@@ -10,7 +12,25 @@ public enum HashAlgorithm {
 	/**
 	 * The password hashing algorithm Argon2id. See https://github.com/P-H-C/phc-winner-argon2
 	 */
-	ARGON2ID(1);
+	ARGON2ID(1) {
+		Argon2 argon = Argon2Factory.create(Argon2Factory.Argon2Types.ARGON2id);
+
+		int memoryInKiB = 488281; // = 500MB
+		int parallelism = Runtime.getRuntime().availableProcessors();
+		long maxMillis = 2000;
+		int iterations = Argon2Helper.findIterations(argon, maxMillis, memoryInKiB, parallelism);
+
+		@Override
+		public String generateHash(AuthToken token) {
+			char[] tokenChars = token.getToken().toCharArray();
+			return argon.hash(iterations, memoryInKiB, parallelism, tokenChars);
+		}
+
+		@Override
+		public boolean matches(String hash, AuthToken token) {
+			return argon.verify(hash, token.getToken().toCharArray());
+		}
+	};
 
 	private final int id;
 
@@ -23,6 +43,40 @@ public enum HashAlgorithm {
 	 */
 	public int getId() {
 		return id;
+	}
+
+	/**
+	 * Generates the appropriate hash for the given token
+	 *
+	 * @param token the token
+	 * @return the hash
+	 */
+	public abstract String generateHash(AuthToken token);
+
+	/**
+	 * Checks whether nor not the provided token verifies against the provided hash.
+	 *
+	 * @param hash the hash
+	 * @param token the token
+	 * @return returns true if the token verifies against the hash
+	 */
+	public abstract boolean matches(String hash, AuthToken token);
+
+	/**
+	 * Tries to find the hash algorithm corresponding to the specified id
+	 *
+	 * @param hashAlgoId the id of the hash algorithm
+	 * @return the hash algorithm, if found
+	 * @throws IllegalArgumentException if no hash algorithm was found for the specified id
+	 */
+	public static HashAlgorithm fromId(int hashAlgoId) {
+		for (HashAlgorithm algo : HashAlgorithm.values()) {
+			if (algo.getId() == hashAlgoId) {
+				return algo;
+			}
+		}
+
+		throw new IllegalArgumentException("invalid hash algo id: " + hashAlgoId);
 	}
 
 }
--- backend/backend/src/main/java/de/aaaaaaah/velcom/backend/access/token/TokenAccess.java
+++ backend/backend/src/main/java/de/aaaaaaah/velcom/backend/access/token/TokenAccess.java
@@ -1,10 +1,15 @@
 package de.aaaaaaah.velcom.backend.access.token;
 
+import static org.jooq.codegen.db.tables.RepoToken.REPO_TOKEN;
+import static org.jooq.codegen.db.tables.Repository.REPOSITORY;
+
 import de.aaaaaaah.velcom.backend.access.AccessLayer;
 import de.aaaaaaah.velcom.backend.access.commit.CommitAccess;
 import de.aaaaaaah.velcom.backend.access.repo.RepoId;
 import de.aaaaaaah.velcom.backend.storage.db.DatabaseStorage;
 import javax.annotation.Nullable;
+import org.jooq.DSLContext;
+import org.jooq.codegen.db.tables.records.RepoTokenRecord;
 
 /**
  * This class abstracts away access to the {@link AuthToken}s which are stored in the db.
@@ -13,9 +18,12 @@ import javax.annotation.Nullable;
  */
 public class TokenAccess {
 
+	private static final HashAlgorithm HASH_ALGORITHM = HashAlgorithm.ARGON2ID;
+
 	private final AccessLayer accessLayer;
 	private final DatabaseStorage databaseStorage;
-	private final AuthToken adminToken;
+
+	private final String adminTokenHash;
 
 	/**
 	 * This constructor also registers the {@link CommitAccess} in the accessLayer.
@@ -29,7 +37,7 @@ public class TokenAccess {
 
 		this.accessLayer = accessLayer;
 		this.databaseStorage = databaseStorage;
-		this.adminToken = adminToken;
+		this.adminTokenHash = HASH_ALGORITHM.generateHash(adminToken);
 
 		accessLayer.registerTokenAccess(this);
 	}
@@ -43,8 +51,39 @@ public class TokenAccess {
 	 * 	not exist or has no token
 	 */
 	public boolean isValidToken(RepoId repoId, AuthToken token) {
-		// TODO implement
-		return false;
+		try (DSLContext db = databaseStorage.acquireContext()) {
+			RepoTokenRecord tokenRecord = db.selectFrom(REPO_TOKEN)
+				.where(REPO_TOKEN.REPO_ID.eq(repoId.getId().toString()))
+				.and(REPO_TOKEN.TOKEN.eq(token.getToken()))
+				.fetchOptional()
+				.orElse(null);
+
+			if (tokenRecord == null) {
+				return false; // no entry in database => default to false
+			}
+
+			String hash = tokenRecord.getToken();
+			int usedAlgoId = tokenRecord.getHashAlgo();
+
+			if (usedAlgoId != HASH_ALGORITHM.getId()) {
+				// Older hash algorithm was used for this token
+				HashAlgorithm usedHashAlgo = HashAlgorithm.fromId(usedAlgoId);
+
+				// Check if provided token is even correct
+				if (!usedHashAlgo.matches(hash, token)) {
+					return false; // Can't migrate if provided token is incorrect
+				}
+
+				// Overwrite hash with newer (currently used) hash algorithm
+				setToken(repoId, token);
+
+				// Since provided token was correct, return true
+				return true;
+			} else {
+				// Current hash algorithm is used
+				return HASH_ALGORITHM.matches(hash, token);
+			}
+		}
 	}
 
 	/**
@@ -52,14 +91,25 @@ public class TokenAccess {
 	 * @return whether the token should give admin powers
 	 */
 	public boolean isValidAdminToken(AuthToken token) {
-		// TODO implement
-		return false;
+		return HASH_ALGORITHM.matches(adminTokenHash, token);
 	}
 
 	// Modifying tokens
 
 	public void setToken(RepoId id, @Nullable AuthToken authToken) {
-		// TODO implement
+		String hash = HASH_ALGORITHM.generateHash(authToken);
+
+		// Insert hash into database
+		try (DSLContext db = databaseStorage.acquireContext()) {
+			db.insertInto(REPO_TOKEN)
+				.set(REPO_TOKEN.REPO_ID, id.getId().toString())
+				.set(REPO_TOKEN.HASH_ALGO, HASH_ALGORITHM.getId())
+				.set(REPO_TOKEN.TOKEN, hash)
+				.onDuplicateKeyUpdate()
+				.set(REPO_TOKEN.HASH_ALGO, HASH_ALGORITHM.getId())
+				.set(REPO_TOKEN.TOKEN, hash)
+				.execute();
+		}
 	}
 
 	// Cleanup
@@ -68,6 +118,10 @@ public class TokenAccess {
 	 * Delete all tokens whose corresponding repo doesn't exist (any more).
 	 */
 	public void deleteAllUnused() {
-		// TODO implement
+		try (DSLContext db = databaseStorage.acquireContext()) {
+			db.deleteFrom(REPO_TOKEN)
+				.where(REPO_TOKEN.REPO_ID.notIn(db.select(REPOSITORY.ID).from(REPOSITORY)));
+		}
 	}
+
 }
