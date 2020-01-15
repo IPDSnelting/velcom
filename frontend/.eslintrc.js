ab9bb3072bcd11f3ed33b352965ba02a233e949b
oudemia <oudemia@posteo.de>
2020-01-15 21:12:52 +0100

    adaptions to simplify repoModule and colorModule working together

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -3,14 +3,12 @@ module.exports = {
   env: {
     node: true
   },
-  'extends': [
-    'plugin:vue/essential',
-    '@vue/standard',
-    '@vue/typescript'
-  ],
+  extends: ['plugin:vue/essential', '@vue/standard', '@vue/typescript'],
   rules: {
-//    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    //    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'space-before-function-paren': ['error', 'never'],
+    'no-new': 0
   },
   parserOptions: {
     parser: '@typescript-eslint/parser'
