268ee0fc88e77495feaa1e1f628c18cd4aef9019
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2020-01-16 00:11:55 +0100

    Show drawer if screen size is too small for navigation icons

--- frontend/src/components/NavigationBar.vue
+++ frontend/src/components/NavigationBar.vue
@@ -1,17 +1,47 @@
 <template>
   <nav>
     <v-toolbar dark color="primary darken-1">
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon class="hidden-md-and-up" @click="drawerShown = !drawerShown"></v-app-bar-nav-icon>
       <v-toolbar-title>{{ title }}</v-toolbar-title>
 
       <v-spacer></v-spacer>
 
       <!-- Navigation items -->
-      <v-btn v-for="item in validRoutes" :key="item.routeName" text :to="{ name: item.routeName }">
+      <v-btn
+        class="hidden-sm-and-down"
+        v-for="item in validRoutes"
+        :key="item.routeName"
+        text
+        :to="{ name: item.routeName }"
+      >
         {{ item.label }}
         <v-icon right dark :size="iconFontSize">{{ item.icon }}</v-icon>
       </v-btn>
     </v-toolbar>
+    <!-- Navigation drawer -->
+    <v-navigation-drawer class="hidden-md-and-up" v-model="drawerShown" app temporary>
+      <v-toolbar dark color="primary darken-1">
+        <v-list>
+          <v-list-item>
+            <v-list-item-title class="title">Navigation</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-toolbar>
+
+      <v-divider></v-divider>
+
+      <v-list dense class="pt-0">
+        <v-list-item v-for="item in validRoutes" :key="item.routeName" :to="item.routeName">
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.label }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
   </nav>
 </template>
 
@@ -37,6 +67,8 @@ class NavigationItem {
 export default class NavigationBar extends Vue {
   private iconFontSize = 22
   private title = 'VelCom'
+
+  private drawerShown = false
 
   get validRoutes() {
     return this.$router.routes
