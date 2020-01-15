a36e286498d9448e3855b841625328940a48c93b
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2020-01-15 23:50:17 +0100

    Use router routes for NavigationBar

--- frontend/src/components/NavigationBar.vue
+++ frontend/src/components/NavigationBar.vue
@@ -1,18 +1,13 @@
 <template>
   <nav>
     <v-toolbar dark color="primary darken-1">
-      <v-app-bar-nav-icon v-if="!actionsHidden" @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
       <v-toolbar-title>{{ title }}</v-toolbar-title>
 
       <v-spacer></v-spacer>
 
       <!-- Navigation items -->
-      <v-btn
-        v-for="item in navigationItems"
-        :key="item.routeName"
-        text
-        :to="{ name: item.routeName }"
-      >
+      <v-btn v-for="item in validRoutes" :key="item.routeName" text :to="{ name: item.routeName }">
         {{ item.label }}
         <v-icon right dark :size="iconFontSize">{{ item.icon }}</v-icon>
       </v-btn>
@@ -24,12 +19,7 @@
 import Vue from 'vue'
 import Component from 'vue-class-component'
 import { VuetifyIcon } from 'vuetify/types/services/icons'
-import {
-  mdiInformationOutline,
-  mdiHome,
-  mdiScaleBalance,
-  mdiSourceBranch
-} from '@mdi/js'
+import { VueRouterEx, RouteConfig } from 'vue-router/types/router'
 
 class NavigationItem {
   readonly routeName: String
@@ -45,13 +35,17 @@ class NavigationItem {
 
 @Component
 export default class NavigationBar extends Vue {
-  private navigationItems: NavigationItem[] = [
-    new NavigationItem('home', mdiHome, 'Home'),
-    new NavigationItem('repo-comparison', mdiScaleBalance, 'Repo Comparison'),
-    new NavigationItem('repo-detail', mdiSourceBranch, 'Repo Detail'),
-    new NavigationItem('about', mdiInformationOutline, 'About')
-  ]
   private iconFontSize = 22
+  private title = 'VelCom'
+
+  get validRoutes() {
+    return this.$router.routes
+      .filter(route => route.meta.navigable)
+      .map(
+        route =>
+          new NavigationItem(route.name!, route.meta.icon, route.meta.label)
+      )
+  }
 }
 </script>
 
--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,7 +1,13 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
+import VueRouter, { RouteConfig, RouterOptions } from 'vue-router'
 import Home from '../views/Home.vue'
 import About from '../views/About.vue'
+import {
+  mdiHome,
+  mdiScaleBalance,
+  mdiSourceBranch,
+  mdiInformationOutline
+} from '@mdi/js'
 
 Vue.use(VueRouter)
 
@@ -11,17 +17,32 @@ const routes = [
     redirect: '/home',
     path: '/',
     name: 'home',
-    component: Home
+    component: Home,
+    meta: {
+      label: 'Home',
+      navigable: true,
+      icon: mdiHome
+    }
   },
   {
     path: '/repo-comparison',
     name: 'repo-comparison',
-    component: About
+    component: About,
+    meta: {
+      label: 'Repo Comparison',
+      navigable: true,
+      icon: mdiScaleBalance
+    }
   },
   {
     path: '/repo-detail',
     name: 'repo-detail',
-    component: About
+    component: About,
+    meta: {
+      label: 'Repo Detail',
+      navigable: true,
+      icon: mdiSourceBranch
+    }
   },
   {
     path: '/about',
@@ -30,11 +51,37 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ '../views/About.vue')
+      import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    meta: {
+      label: 'About',
+      navigable: true,
+      icon: mdiInformationOutline
+    }
   }
 ]
 
-const router = new VueRouter({
+class VueRouterEx extends VueRouter {
+  matcher: any
+
+  public routes: RouteConfig[] = []
+
+  constructor(options: RouterOptions) {
+    super(options)
+    const { addRoutes } = this.matcher
+    const { routes } = options
+
+    this.routes = routes!
+
+    this.matcher.addRoutes = (newRoutes: RouteConfig[]) => {
+      this.routes.push(...newRoutes)
+      addRoutes(newRoutes)
+    }
+  }
+}
+
+Vue.use(VueRouterEx)
+
+const router = new VueRouterEx({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
