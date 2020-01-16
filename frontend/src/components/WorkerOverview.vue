0da5fe2b1ab6abfa2785dba5be8e71806484a2fc
oudemia <oudemia@posteo.de>
2020-01-16 20:50:28 +0100

    adapt dummy Workers in WorkerOverview

--- frontend/src/components/WorkerOverview.vue
+++ frontend/src/components/WorkerOverview.vue
@@ -5,14 +5,20 @@
         <v-card v-if="!dense">
           <v-card-title>{{ worker.name }}</v-card-title>
           <v-card-text>
-            <span class="worker-description">{{ formatWorkerInformation(worker) }}</span>
+            <span class="worker-description">{{
+              formatWorkerInformation(worker)
+            }}</span>
           </v-card-text>
         </v-card>
         <v-tooltip bottom>
           <template #activator="{ on }">
-            <v-chip outlined label color="accent" v-on="on" v-if="dense">{{ worker.name }}</v-chip>
+            <v-chip outlined label color="accent" v-on="on" v-if="dense">{{
+              worker.name
+            }}</v-chip>
           </template>
-          <span class="worker-description">{{ formatWorkerInformation(worker) }}</span>
+          <span class="worker-description">{{
+            formatWorkerInformation(worker)
+          }}</span>
         </v-tooltip>
       </v-col>
     </v-row>
@@ -22,7 +28,7 @@
 <script lang="ts">
 import Vue from 'vue'
 import Component from 'vue-class-component'
-import { Worker, RootState, QueueState } from '../store/types'
+import { Worker, Commit, RootState, QueueState } from '../store/types'
 import { Prop } from 'vue-property-decorator'
 import { Store } from 'vuex'
 
@@ -32,15 +38,45 @@ export default class WorkerOverview extends Vue {
     default: () => [
       new Worker(
         'Test Runner',
-        'Linux amd64 4.19.94-1-lts, 8 cores, 4167MB max RAM'
+        'Linux amd64 4.19.94-1-lts, 8 cores, 4167MB max RAM',
+        new Commit(
+          'repo1',
+          'iAmAHash',
+          'author',
+          123,
+          'committer',
+          123,
+          'I am a message',
+          []
+        )
       ),
       new Worker(
         'Test Runner 1',
-        'Linux amd64dsdsdsds 4.19.94-1-lts, 8 cores, 4167MB max RAM'
+        'Linux amd64dsdsdsds 4.19.94-1-lts, 8 cores, 4167MB max RAM',
+        new Commit(
+          'repo1',
+          'iAmASecondHash',
+          'author',
+          125,
+          'committer',
+          125,
+          'I am a second message',
+          ['parentHash']
+        )
       ),
       new Worker(
         'Test Runner 2',
-        'Linux amd64 4.19.94dsdsdsds-1-lts, 8 cores, 4167MB max RAM'
+        'Linux amd64 4.19.94dsdsdsds-1-lts, 8 cores, 4167MB max RAM',
+        new Commit(
+          'repo2',
+          'iAmAThirdHash',
+          'author',
+          127,
+          'committer',
+          127,
+          'I am a third message',
+          ['parent1hash', 'parent2hash']
+        )
       )
     ]
   })
