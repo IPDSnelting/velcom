3ff2652085c216074d0cd7b8ae8ecb831e140272
oudemia <oudemia@posteo.de>
2020-01-16 20:20:45 +0100

    correct mistake in fetchRepoByID action

--- frontend/src/store/modules/repoModule/actions.ts
+++ frontend/src/store/modules/repoModule/actions.ts
@@ -67,11 +67,27 @@ export const actions: ActionTree<RepoState, RootState> = {
           token: payload.repoToken
         }
       })
-      .then(response => {
-        const repo: Repo = response.data
-        commit('SET_REPO', repo)
-        return repo
-      })
+      .then(
+        response => {
+          let repo = response.data.map((item: any) => {
+            new Repo(
+              item.id,
+              item.name,
+              item.branches,
+              item.tracked_branches,
+              item.measurements,
+              item.remote_url
+            )
+          })
+
+          commit('SET_REPO', repo)
+          return repo
+        },
+        error => {
+          console.log('error: could not add new repo ' + payload.name)
+          console.log(error)
+        }
+      )
   },
 
   deleteRepo({ commit, rootGetters }, repoID: string) {
