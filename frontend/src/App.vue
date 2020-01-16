928690b35d6e7779496c7520c34690028688c539
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2020-01-16 20:05:02 +0100

    Add test components to App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,6 +2,13 @@
   <v-app>
     <v-content>
       <nav-bar></nav-bar>
+      <snackbar></snackbar>
+      <repo-add>
+        <template #activator="{ on }">
+          <v-btn v-on="on">Test</v-btn>
+        </template>
+      </repo-add>
+      <worker-overview></worker-overview>
       <router-view></router-view>
       <color-module-tester></color-module-tester>
       <repo-module-tester></repo-module-tester>
@@ -15,12 +22,18 @@ import ColorModuleTester from './testComponents/ColorModuleTester.vue'
 import RepoModuleTester from './testComponents/RepoModuleTester.vue'
 import Component from 'vue-class-component'
 import NavigationBar from './components/NavigationBar.vue'
+import RepoAddDialog from './components/RepoAddDialog.vue'
+import WorkerOverview from './components/WorkerOverview.vue'
+import Snackbar from './components/Snackbar.vue'
 
 @Component({
   components: {
     'color-module-tester': ColorModuleTester,
     'repo-module-tester': RepoModuleTester,
-    'nav-bar': NavigationBar
+    'nav-bar': NavigationBar,
+    'repo-add': RepoAddDialog,
+    'worker-overview': WorkerOverview,
+    snackbar: Snackbar
   }
 })
 export default class App extends Vue {}
