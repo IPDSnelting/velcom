2e18d280c56e408b107d757cfced72def954326a
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2020-01-18 15:27:46 +0100

    Make new store default

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,11 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
+import { store, vxm } from './store/classIndex'
 import axios from 'axios'
 import vuetify from './plugins/vuetify'
 import { extractErrorMessage } from './util/ErrorUtils'
-import { vxm } from './store/classIndex'
 
 Vue.config.productionTip = false
 
