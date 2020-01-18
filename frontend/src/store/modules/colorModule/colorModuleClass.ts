b88982a99c6a3c0cb4d2b3a52b59db762fb354ae
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2020-01-18 15:08:50 +0100

    Convert color module to class syntax

---
+++ frontend/src/store/modules/colorModule/colorModuleClass.ts
@@ -0,0 +1,85 @@
+import { createModule, mutation, action } from 'vuex-class-component'
+import { CommitComparison, ColorConverter } from '@/store/types'
+
+const VxModule = createModule({
+  namespaced: 'colorModule',
+  strict: false
+})
+
+export class ColorModuleStore extends VxModule {
+  /* see the muted qualitative colour scheme on
+   * https://personal.sron.nl/~pault/#sec:qualitative
+   */
+  private colors: string[] = [
+    '#332288',
+    '#88CCEE',
+    '#44AA99',
+    '#117733',
+    '#999933',
+    '#DDCC77',
+    '#CC6677',
+    '#882255',
+    '#AA4499'
+  ]
+
+  /**
+   * Generates the given amount of new colors and adds them
+   * to the store.
+   *
+   * @param {number} amount the number of colors to generate
+   * @memberof ColorModuleStore
+   */
+  @mutation
+  addColors(amount: number) {
+    // generating new colors in hsl color space using golden ratio to maximize difference
+    var converter = new ColorConverter()
+    const colors = this.colors
+
+    const phi = 1.6180339887
+    const saturation = 0.5
+    const lightness = 0.5
+
+    for (let i = 0; i < amount; i++) {
+      const lastColor = colors[colors.length - 1]
+      var hue = converter.hexToHsl(lastColor)[0]
+
+      hue += phi
+      hue %= 1
+      const newColor = converter.hslToHex(hue, saturation, lightness)
+
+      this.addColor(newColor)
+    }
+  }
+
+  /**
+   * Adds a new color.
+   *
+   * @param {string} payload the color to add
+   * @memberof ColorModuleStore
+   */
+  @mutation
+  addColor(payload: string) {
+    this.colors.push(payload)
+  }
+
+  /**
+   * Returns all colors.
+   *
+   * @readonly
+   * @type {string[]}
+   * @memberof ColorModuleStore
+   */
+  get allColors(): string[] {
+    return this.colors
+  }
+
+  /**
+   * Returns a color by its index.
+   *
+   * @readonly
+   * @memberof ColorModuleStore
+   */
+  get colorByIndex(): (index: number) => string {
+    return (index: number) => this.colors[index]
+  }
+}
