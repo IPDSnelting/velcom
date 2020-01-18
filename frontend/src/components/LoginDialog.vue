3456214b57738183b587708a87a2fe4a0901422d
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2020-01-18 15:57:31 +0100

    Beautify login repo selection

--- frontend/src/components/LoginDialog.vue
+++ frontend/src/components/LoginDialog.vue
@@ -17,12 +17,25 @@
               </template>
               <v-radio v-for="role in roles" :key="role" :label="role" :value="role"></v-radio>
             </v-radio-group>
-            <v-text-field
-              :disabled="!this.idRequired"
+            <v-autocomplete
+              :disabled="!idRequired"
+              :items="allRepos"
               :rules="[nonEmptyID]"
-              label="*Repository name"
               v-model="repoID"
-            ></v-text-field>
+              item-text="name"
+              item-value="id"
+              label="*Repository name"
+            >
+              <template v-slot:item="data">
+                <v-list-item-avatar>
+                  <v-icon small>{{ repoIcon }}</v-icon>
+                </v-list-item-avatar>
+                <v-list-item-content>
+                  <v-list-item-title>{{ data.item.name }}</v-list-item-title>
+                  <v-list-item-subtitle>{{ data.item.id }}</v-list-item-subtitle>
+                </v-list-item-content>
+              </template>
+            </v-autocomplete>
             <v-text-field :rules="[nonEmptyToken]" label="Access token" v-model="token"></v-text-field>
           </v-form>
         </v-card-text>
@@ -41,26 +54,33 @@
 import Vue from 'vue'
 import { Component, Watch } from 'vue-property-decorator'
 import { Store } from 'vuex'
-import { RootState } from '../store/types'
+import { RootState, Repo } from '../store/types'
 import { extractErrorMessage } from '../util/ErrorUtils'
 import { store, vxm } from '../store/classIndex'
+import { mdiSourceBranch } from '@mdi/js'
 
 @Component
 export default class LoginDialog extends Vue {
   private repoID: string = ''
   private token: string = ''
 
-  private roles: Array<string> = ['Web-Admin', 'Repository-Admin']
+  private roles: string[] = ['Web-Admin', 'Repository-Admin']
   private role: string = this.roles[0]
 
   private formValid: boolean = false
   private dialogOpen: boolean = false
 
-  private error: string = ''
-
   @Watch('role')
   private onRoleChange() {
     ;(this.$refs.form as any).validate()
+  }
+
+  @Watch('dialogOpen')
+  private onOpened(opened: boolean) {
+    if (!opened) {
+      this.token = ''
+      this.repoID = ''
+    }
   }
 
   get idRequired(): boolean {
@@ -77,8 +97,12 @@ export default class LoginDialog extends Vue {
     return input.length > 0 ? true : 'This field must not be empty!'
   }
 
+  private get allRepos(): Repo[] {
+    return vxm.repoModule.allRepos
+  }
+
   private login() {
-    var payload: {
+    let payload: {
       role: string
       asRepoAdmin: boolean
       token: string
@@ -99,6 +123,10 @@ export default class LoginDialog extends Vue {
     }
     vxm.userModule.logIn(payload).then(() => (this.dialogOpen = false))
   }
+
+  // ============== ICONS ==============
+  private repoIcon = mdiSourceBranch
+  // ==============       ==============
 }
 </script>
 
