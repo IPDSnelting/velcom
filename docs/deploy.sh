783a7aa8e4f1e35db01e704c4627233db56e3c83
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2020-01-22 13:00:07 +0100

    Try to add deploy step

---
+++ docs/deploy.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+function copyToServer() {
+    echo "Copying $1 to $CD_URL at location $2"
+    scp -P "$CD_PORT" "$0" "$CD_USER@$CD_URL" "$1"
+}
+
+function makeDirOnServer() {
+    echo "Making dir $1"
+    ssh -p "$CD_PORT" "$CD_USER@$CD_URL" "mkdir -p $0"
+}
+
+makeDirOnServer "/home/pse_test/velcom"
+
+copyToServer "backend/backend/target/backend.jar" "/home/pse_test/velcom"
+copyToServer "backend/runner/target/backend.jar" "/home/pse_test/velcom"
+tar -cf dist.tar "frontend/dist"
+copyToServer "dist.tar" "/home/pse_test/velcom"
