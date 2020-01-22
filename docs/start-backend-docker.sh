8f43ba8c50aa9246364ce8fd0be8bf650ae835bc
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2020-01-22 20:25:17 +0100

    Correctly restart container

--- docs/start-backend-docker.sh
+++ docs/start-backend-docker.sh
@@ -1,4 +1,14 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
+# Start nginx for routing
 nginx &
-java -jar /home/velcom/velcom.jar server "$1"
+
+java -jar /home/velcom/velcom.jar server "$1" &
+JAVA_PID="$!"
+
+# The shell does not pass on exit signals, so we do it manually
+# This ensures java is killed properly and the container can restart.
+trap "kill $JAVA_PID" exit INT TERM
+
+# Wait for the subprocesses (& spawned) to finish
+wait
