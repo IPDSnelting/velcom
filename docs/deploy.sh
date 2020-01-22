03bef7405b75e639810220203e4d49db989db445
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2020-01-22 14:51:09 +0100

    Fix copy scp path

--- docs/deploy.sh
+++ docs/deploy.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 function copyToServer() {
     echo "Copying $1 to $CD_URL at location $2"
-    scp -P "$CD_PORT" "$1" "$CD_USER@$CD_URL" "$2"
+    scp -P "$CD_PORT" "$1" "$CD_USER@$CD_URL:$2"
 }
 
 function makeDirOnServer() {
     echo "Making dir $1"
-    echo "ssh -p $CD_PORT $CD_USER@$CD_URL 'mkdir -p $1'"
     ssh -p "$CD_PORT" "$CD_USER@$CD_URL" "mkdir -p $1"
 }
 
