d608b2130f3aae4c408c38434ab5261523210a84
Joscha <joscha@plugh.de>
2020-01-24 20:54:33 +0000

    Adjust db schema
    Also, the build target for backend.jar now also checks if any db schema files
    have changed.

--- backend/backend/src/main/resources/db/migration/V1__create_initial_tables.sql
+++ backend/backend/src/main/resources/db/migration/V1__create_initial_tables.sql
@@ -11,7 +11,7 @@ CREATE TABLE tracked_branch
     branch_name TEXT     NOT NULL,
 
     PRIMARY KEY (repo_id, branch_name),
-    FOREIGN KEY (repo_id) REFERENCES repository (id)
+    FOREIGN KEY (repo_id) REFERENCES repository (id) ON DELETE CASCADE
 );
 
 CREATE TABLE known_commit
@@ -23,7 +23,7 @@ CREATE TABLE known_commit
     insert_time TIMESTAMP NOT NULL,
 
     PRIMARY KEY (repo_id, hash),
-    FOREIGN KEY (repo_id) REFERENCES repository (id)
+    FOREIGN KEY (repo_id) REFERENCES repository (id) ON DELETE CASCADE
 );
 
 CREATE TABLE run
@@ -35,7 +35,7 @@ CREATE TABLE run
     stop_time     TIMESTAMP            NOT NULL,
     error_message TEXT,
 
-    FOREIGN KEY (repo_id, commit_hash) REFERENCES known_commit (repo_id, hash)
+    FOREIGN KEY (repo_id, commit_hash) REFERENCES known_commit (repo_id, hash) ON DELETE CASCADE
 );
 
 CREATE TABLE run_measurement
@@ -48,7 +48,7 @@ CREATE TABLE run_measurement
     interpretation TEXT,
     error_message  TEXT,
 
-    FOREIGN KEY (run_id) REFERENCES run (id)
+    FOREIGN KEY (run_id) REFERENCES run (id) ON DELETE CASCADE
 );
 
 CREATE TABLE run_measurement_value
@@ -56,7 +56,7 @@ CREATE TABLE run_measurement_value
     measurement_id CHAR(36) NOT NULL,
     value          DOUBLE   NOT NULL,
 
-    FOREIGN KEY (measurement_id) REFERENCES run_measurement (id)
+    FOREIGN KEY (measurement_id) REFERENCES run_measurement (id) ON DELETE CASCADE
 );
 
 CREATE TABLE repo_token
@@ -65,5 +65,5 @@ CREATE TABLE repo_token
     token     TEXT                 NOT NULL,
     hash_algo INTEGER              NOT NULL,
 
-    FOREIGN KEY (repo_id) REFERENCES repository (id)
+    FOREIGN KEY (repo_id) REFERENCES repository (id) ON DELETE CASCADE
 );
