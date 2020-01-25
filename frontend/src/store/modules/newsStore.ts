86bcd36aed865332ca887053365ed2104e99a974
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2020-01-25 19:47:44 +0100

    Fix newsStore by using the dedicated json helpers

--- frontend/src/store/modules/newsStore.ts
+++ frontend/src/store/modules/newsStore.ts
@@ -8,6 +8,10 @@ import {
   Difference
 } from '@/store/types'
 import axios from 'axios'
+import {
+  commitFromJson,
+  comparisonFromJson
+} from '@/util/CommitComparisonJsonHelper'
 
 const VxModule = createModule({
   namespaced: 'newsModule',
@@ -30,95 +34,11 @@ export class NewsStore extends VxModule {
       }
     })
 
-    let comparisons: CommitComparison[] = []
     let jsonComparisons: any[] = response.data.commits
 
-    jsonComparisons.forEach((comparison: any) => {
-      let firstCommit: Commit = new Commit(
-        comparison.first.commit.repo_id,
-        comparison.first.commit.hash,
-        comparison.first.commit.author,
-        comparison.first.commit.author_date,
-        comparison.first.commit.committer,
-        comparison.first.commit.committer_date,
-        comparison.first.commit.message,
-        comparison.first.commit.parents
-      )
-      let secondCommit: Commit = new Commit(
-        comparison.second.commit.repo_id,
-        comparison.second.commit.hash,
-        comparison.second.commit.author,
-        comparison.second.commit.author_date,
-        comparison.second.commit.committer,
-        comparison.second.commit.committer_date,
-        comparison.second.commit.message,
-        comparison.second.commit.parents
-      )
-
-      let firstMeasurements: Measurement[] = []
-      if (comparison.first.measurements) {
-        comparison.first.measurements.forEach((measurement: any) => {
-          let id: MeasurementID = new MeasurementID(
-            measurement.benchmark,
-            measurement.metric
-          )
-          firstMeasurements.push(
-            new Measurement(
-              id,
-              measurement.unit,
-              measurement.interpretation,
-              measurement.values,
-              measurement.value,
-              measurement.error_message
-            )
-          )
-        })
-      }
-      let secondMeasurements: Measurement[] = []
-      if (comparison.second.measurements) {
-        comparison.second.measurements.forEach((measurement: any) => {
-          let id: MeasurementID = new MeasurementID(
-            measurement.benchmark,
-            measurement.metric
-          )
-          firstMeasurements.push(
-            new Measurement(
-              id,
-              measurement.unit,
-              measurement.interpretation,
-              measurement.values,
-              measurement.value,
-              measurement.error_message
-            )
-          )
-        })
-      }
-
-      let first = new Run(
-        firstCommit,
-        comparison.first.start_time,
-        comparison.first.stop_time,
-        firstMeasurements,
-        comparison.first.error_message
-      )
-      let second = new Run(
-        secondCommit,
-        comparison.second.start_time,
-        comparison.second.stop_time,
-        secondMeasurements,
-        comparison.second.error_message
-      )
-
-      let differences: Difference[] = []
-      comparison.differences.forEach((difference: any) => {
-        let measurement = new MeasurementID(
-          difference.benchmark,
-          difference.metric
-        )
-        differences.push(new Difference(measurement, difference.difference))
-      })
-      comparisons.push(new CommitComparison(first, second, differences))
-    })
+    let comparisons: CommitComparison[] = jsonComparisons.map(it =>
+      comparisonFromJson(it)
+    )
 
     return comparisons
   }
