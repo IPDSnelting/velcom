fbcbed2b22117d88022d0612fc26d1edbd5f8b76
kwerber <werberkevin@gmail.com>
2020-01-26 11:34:07 +0100

    Add branch concept to TestCommit&TestRepo

--- backend/backend/src/test/java/de/aaaaaaah/velcom/backend/TestCommit.java
+++ backend/backend/src/test/java/de/aaaaaaah/velcom/backend/TestCommit.java
@@ -1,15 +1,27 @@
 package de.aaaaaaah.velcom.backend;
 
+import javax.annotation.Nullable;
+
 public class TestCommit {
 
 	private final String message;
 	private final String file;
 	private final String content;
+	private final String branch;
+
+	public TestCommit(String message) {
+		this(message, "afile.txt", String.valueOf(System.currentTimeMillis()));
+	}
 
 	public TestCommit(String message, String file, String content) {
+		this(message, file, content, null);
+	}
+
+	public TestCommit(String message, String file, String content, @Nullable String branch) {
 		this.message = message;
 		this.file = file;
 		this.content = content;
+		this.branch = branch;
 	}
 
 	public String getMessage() {
@@ -22,6 +34,11 @@ public class TestCommit {
 
 	public String getContent() {
 		return content;
+	}
+
+	@Nullable
+	public String getBranch() {
+		return branch;
 	}
 
 	@Override
--- backend/backend/src/test/java/de/aaaaaaah/velcom/backend/TestRepo.java
+++ backend/backend/src/test/java/de/aaaaaaah/velcom/backend/TestRepo.java
@@ -4,7 +4,9 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.revwalk.RevCommit;
@@ -12,9 +14,13 @@ import org.eclipse.jgit.revwalk.RevCommit;
 public class TestRepo {
 
 	private final Path repoDir;
+	private final Set<String> branches = new HashSet<>();
+	private String currentBranch = "master";
 
 	public TestRepo(Path repoDir) throws GitAPIException, IOException {
 		this.repoDir = repoDir;
+		this.branches.add("master");
+
 		if (!Files.exists(repoDir)) {
 			Files.createDirectory(repoDir);
 		}
@@ -29,15 +35,40 @@ public class TestRepo {
 	}
 
 	public RevCommit commit(TestCommit commit) throws IOException, GitAPIException {
-		return commit(commit.getMessage(), commit.getFile(), commit.getContent());
+		if (commit.getBranch() == null) {
+			return commit(commit.getMessage(), commit.getFile(), commit.getContent());
+		} else {
+			return commit(commit.getMessage(), commit.getFile(), commit.getContent(),
+				commit.getBranch());
+		}
 	}
 
 	public RevCommit commit(String message, String file, String content)
 		throws IOException, GitAPIException {
+		return this.commit(message, file, content, "master");
+	}
+
+	public RevCommit commit(String message, String file, String content, String branch)
+		throws IOException, GitAPIException {
 
 		try (Git git = Git.open(repoDir.toFile())) {
+			if (!currentBranch.equals(branch)) {
+				if (branches.contains(branch)) {
+					// branch already exists
+					git.checkout().setName(branch).call();
+					currentBranch = branch;
+				} else {
+					// new branch
+					git.checkout().setName(branch).setCreateBranch(true).call();
+					branches.add(branch);
+					currentBranch = branch;
+				}
+			}
+
 			Files.write(repoDir.resolve(file), content.getBytes(), StandardOpenOption.CREATE);
+
 			git.add().addFilepattern(file).call();
+
 			return git.commit()
 				.setAuthor("Peter", "peter@email.com")
 				.setCommitter("Peter", "peter@email.com")
