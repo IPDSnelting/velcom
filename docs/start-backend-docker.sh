fa7bd6a64eada04c1bf9276f17993d5466e3e9f0
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2020-01-26 10:49:07 +0100

    Document docker entrypoint script

--- docs/start-backend-docker.sh
+++ docs/start-backend-docker.sh
@@ -1,14 +1,25 @@
 #!/usr/bin/env bash
 
+# This is a sample docker entrypoint. It is used by the provided docker image
+# to start up the backend.
+
+# Relevant parts:
+#  1. Start nginx AND our application
+#  2. Relay SIGTERM and SIGINT to our java application so the docker container
+#     can be stopped properly
+
 # Start nginx for routing
 nginx &
 
+# Start our backend
 java -jar /home/velcom/velcom.jar server "$1" &
+
+# Save its PID so we can relay kill signals
 JAVA_PID="$!"
 
 # The shell does not pass on exit signals, so we do it manually
 # This ensures java is killed properly and the container can restart.
 trap "kill $JAVA_PID" exit INT TERM
 
-# Wait for the subprocesses (& spawned) to finish
+# Wait for the subprocesses (= spawned by &) to finish
 wait
