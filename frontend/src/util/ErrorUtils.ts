f3da81c33eb630b2381807a3d0398a1ea6a1eeae
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2020-01-26 11:47:47 +0100

    Slightly cleanup error message extraction

--- frontend/src/util/ErrorUtils.ts
+++ frontend/src/util/ErrorUtils.ts
@@ -7,12 +7,8 @@ export interface AxiosError {
 
 export function extractErrorMessage(error: AxiosError): string {
   if (error.response) {
-    let errorMessage: null | string = null
-    if (error.response.data.message) {
-      errorMessage = error.response.data.message
-    } else if (error.response.data.error) {
-      errorMessage = error.response.data.error
-    }
+    let errorMessage: undefined | string =
+      error.response.data.message || error.response.data.error
     if (errorMessage) {
       return `${errorMessage} (${error.response.status})`
     } else if (error.response.status === 401 || error.response.status === 403) {
