1038f9af0a5225047257b9d4089f6638b8ae2e2a
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2020-01-28 19:57:39 +0100

    Fix tracked branch chip sorting RepoDetail

--- frontend/src/views/RepoDetail.vue
+++ frontend/src/views/RepoDetail.vue
@@ -24,7 +24,7 @@
               <v-row align="center">
                 <v-col cols="3" class="subtitle-2">Branches:</v-col>
                 <v-col cols="9">
-                  <v-tooltip top v-for="branch in branches" :key="branch">
+                  <v-tooltip top v-for="(branch, index) in branches" :key="branch + index">
                     <template v-slot:activator="{ on }">
                       <v-chip
                         class="ma-2"
