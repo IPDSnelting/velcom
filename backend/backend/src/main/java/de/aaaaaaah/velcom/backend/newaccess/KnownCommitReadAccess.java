00544b37d22d939cb3921cfb4e9d54eda35d3da5
Joscha <joscha@plugh.de>
2020-02-05 11:41:10 +0000

    Extract write functions to KnownCommitWriteAccess

--- backend/backend/src/main/java/de/aaaaaaah/velcom/backend/newaccess/KnownCommitReadAccess.java
+++ backend/backend/src/main/java/de/aaaaaaah/velcom/backend/newaccess/KnownCommitReadAccess.java
@@ -8,21 +8,17 @@ import de.aaaaaaah.velcom.backend.newaccess.entities.CommitHash;
 import de.aaaaaaah.velcom.backend.newaccess.entities.RepoId;
 import de.aaaaaaah.velcom.backend.storage.db.DatabaseStorage;
 import de.aaaaaaah.velcom.backend.util.Pair;
-import java.sql.Timestamp;
-import java.time.Instant;
 import java.util.Collection;
-import java.util.List;
 import java.util.Set;
 import java.util.UUID;
 import java.util.stream.Collectors;
 import org.jooq.DSLContext;
-import org.jooq.InsertValuesStep5;
 import org.jooq.Record1;
 import org.jooq.codegen.db.tables.records.KnownCommitRecord;
 
 public class KnownCommitReadAccess {
 
-	private final DatabaseStorage databaseStorage;
+	final DatabaseStorage databaseStorage;
 
 	public KnownCommitReadAccess(DatabaseStorage databaseStorage) {
 		this.databaseStorage = databaseStorage;
@@ -52,34 +48,6 @@ public class KnownCommitReadAccess {
 				.map(Record1::value1)
 				.map(BenchmarkStatus::fromNumericalValue)
 				.orElse(BenchmarkStatus.BENCHMARK_REQUIRED);
-		}
-	}
-
-	public void setBenchmarkStatus(RepoId repoId, CommitHash commitHash,
-		BenchmarkStatus benchmarkStatus) {
-
-		setBenchmarkStatus(repoId, List.of(commitHash), benchmarkStatus);
-	}
-
-	public void setBenchmarkStatus(RepoId repoId, Collection<CommitHash> commitHashes,
-		BenchmarkStatus benchmarkStatus) {
-
-		try (DSLContext db = databaseStorage.acquireContext()) {
-			final InsertValuesStep5<KnownCommitRecord, String, String, Integer, Timestamp, Timestamp> step = db
-				.insertInto(KNOWN_COMMIT, KNOWN_COMMIT.REPO_ID, KNOWN_COMMIT.HASH,
-					KNOWN_COMMIT.STATUS, KNOWN_COMMIT.UPDATE_TIME, KNOWN_COMMIT.INSERT_TIME);
-
-			int statusInteger = benchmarkStatus.getNumericalValue();
-			Timestamp now = Timestamp.from(Instant.now());
-
-			for (CommitHash hash : commitHashes) {
-				step.values(repoId.getId().toString(), hash.getHash(), statusInteger, now, now);
-			}
-
-			step.onDuplicateKeyUpdate()
-				.set(KNOWN_COMMIT.STATUS, statusInteger)
-				.set(KNOWN_COMMIT.UPDATE_TIME, now)
-				.execute();
 		}
 	}
 
