5863429f9a013d04ea4995f8135a737210e287ca
I-Al-Istannen <I-Al-Istannen@users.noreply.github.com>
2020-06-03 12:37:25 +0200

    Setup Github CI, remove Gitlab CI

--- docs/deploy.sh
+++ docs/deploy.sh
@@ -1,45 +1,63 @@
 #!/usr/bin/env bash
 
-# This is our Continous Deployment deploy script. You may use it as a baseline,
-# but it is likely
-#  a) Way too specific
-#  b) Full of »totally best practices«™
+# This is our Continous Deployment deploy script.
+# It just calls out to the server and requests the docker image to be pulled
+# again and restarts the service.
 
 # Fail if any command inside fails
 set -e
-
-function copyToServer() {
-    echo "Copying $1 to $CD_URL at location $2"
-    scp -P "$CD_PORT" "$1" "$CD_USER@$CD_URL:$2"
-}
 
 function executeOnServer() {
     echo "Executing on server: '$1'"
     ssh -p "$CD_PORT" "$CD_USER@$CD_URL" "$1"
 }
 
-DOCKER_SERVER_DIR="/home/pse_test/velcom/.docker"
+function setup() {
+    ##
+    ## Create the SSH directory and give it the right permissions
+    ##
+    mkdir -p ~/.ssh
+    chmod 700 ~/.ssh
 
-executeOnServer "rm -rf $DOCKER_SERVER_DIR"
-executeOnServer "mkdir -p $DOCKER_SERVER_DIR"
+    ##
+    ## Install ssh-agent if not already installed, it is required by Docker.
+    ## (change apt-get to yum if you use an RPM-based image)
+    ##
+    which ssh-agent || ( apt update -y && apt install openssh-client -y )
 
-copyToServer "backend/backend/target/backend.jar" "$DOCKER_SERVER_DIR"
-copyToServer "backend/runner/target/runner.jar" "$DOCKER_SERVER_DIR"
+    ##
+    ## Run ssh-agent (inside the build environment)
+    ##
+    eval $(ssh-agent -s)
 
-copyToServer "docs/nginx-site" "$DOCKER_SERVER_DIR"
-copyToServer "docs/nginx-site-single-port" "$DOCKER_SERVER_DIR"
-copyToServer "docs/nginx.conf" "$DOCKER_SERVER_DIR"
-copyToServer "docs/start-backend-docker.sh" "$DOCKER_SERVER_DIR"
+    ##
+    ## Add the SSH key stored in SSH_PRIVATE_KEY variable to the agent store
+    ## We're using tr to fix line endings which makes ed25519 keys work
+    ## without extra base64 encoding.
+    ## https://gitlab.com/gitlab-examples/ssh-private-key/issues/1#note_48526556
+    ##
+    echo "$SSH_PRIVATE_KEY" | tr -d '\r' | ssh-add -
 
-tar -cf dist.tar "frontend/dist"
-copyToServer "dist.tar" "$DOCKER_SERVER_DIR"
+    ## Setup host key verification
+    echo "$SSH_KNOWN_HOSTS" > ~/.ssh/known_hosts
+    chmod 644 ~/.ssh/known_hosts
+}
 
-executeOnServer "tar -xf $DOCKER_SERVER_DIR/dist.tar --directory $DOCKER_SERVER_DIR"
-executeOnServer "mv $DOCKER_SERVER_DIR/frontend/dist $DOCKER_SERVER_DIR/dist"
-executeOnServer "rmdir $DOCKER_SERVER_DIR/frontend"
+if [ -z "$1" -o -z "$2" ]; then
+    echo "Usage: $0 <github actor name> <github packes access token>"
+    exit 1
+fi
+
+GITHUB_NAME="$1"
+GITHUB_TOKEN="$2"
+
+setup
+
+# Login to github registry
+executeOnServer "echo "$GITHUB_TOKEN" | sudo docker login docker.pkg.github.com -u "$GITHUB_NAME" --password-stdin"
 
 # Build it
-executeOnServer "sudo /home/pse_test/velcom/deploy_from_docker.sh"
+executeOnServer "sudo /home/velcom/update-docker-image.sh"
 
 # Restart the docker container :)
-executeOnServer "sudo systemctl restart pse-test-velcom.service"
+executeOnServer "sudo systemctl restart velcom.service"
