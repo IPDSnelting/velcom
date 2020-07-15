ed95c4ee6e48fe6cc34b6d4204f0487b5f582dd5
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2020-07-15 17:24:01 +0200

    [frontend] Fix public path settings
    The router uses history mode and therefore needs to painfully aware of
    where the SPA is served from. In a previous commit support for
    deployment at a subpath was introduced, but the BASE_URL parameter in
    the router was missed.
    
    This commit removes the "SITE" parameter introduced earlier and replaces
    it with BASE_URL. This ensures the router is kept in sync with the
    publicPath.

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   transpileDependencies: ['vuetify', 'vue-echarts', 'resize-detector'],
-  publicPath: process.env.SITE === 'kit' ? '/velcom/' : '/'
+  publicPath: process.env.BASE_URL
 }
