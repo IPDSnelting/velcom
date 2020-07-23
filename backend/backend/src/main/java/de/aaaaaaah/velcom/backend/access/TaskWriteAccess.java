ee2edc8828aaadc10d857b7b2d01dfa5a9b8bc7d
kwerber <werberkevin@gmail.com>
2020-07-23 20:29:25 +0200

    [backend] Prevent a commit from being inserted into queue twice
    Also do the whole "is task in db already" check within a transaction.

--- backend/backend/src/main/java/de/aaaaaaah/velcom/backend/access/TaskWriteAccess.java
+++ backend/backend/src/main/java/de/aaaaaaah/velcom/backend/access/TaskWriteAccess.java
@@ -4,6 +4,7 @@ import static java.util.stream.Collectors.toList;
 import static org.jooq.codegen.db.Tables.TASK;
 
 import de.aaaaaaah.velcom.backend.access.entities.CommitHash;
+import de.aaaaaaah.velcom.backend.access.entities.CommitId;
 import de.aaaaaaah.velcom.backend.access.entities.RepoId;
 import de.aaaaaaah.velcom.backend.access.entities.RepoSource;
 import de.aaaaaaah.velcom.backend.access.entities.TarSource;
@@ -14,12 +15,15 @@ import java.sql.Timestamp;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 import java.util.UUID;
 import java.util.function.Consumer;
+import org.jooq.Condition;
 import org.jooq.DSLContext;
+import org.jooq.impl.DSL;
 
 /**
  * Provides write access to task objects stored in a database.
@@ -65,56 +69,101 @@ public class TaskWriteAccess extends TaskReadAccess {
 		insertTasks(tasks, databaseStorage.acquireContext());
 	}
 
-	void insertTasks(Collection<Task> tasks, DSLContext db) {
-		// Get tasks that are already in queue
+	void insertTasks(Collection<Task> originalTasks, DSLContext db) {
+		// Create mutable copy of task list
+		final List<Task> tasks = new ArrayList<>(originalTasks);
+
+		// 1.) Get tasks that are already in queue
 		List<String> idList = tasks.stream()
 			.map(Task::getId)
 			.map(TaskId::getId)
 			.map(UUID::toString)
 			.collect(toList());
 
-		Set<String> existingIds = db.selectFrom(TASK).where(TASK.ID.in(idList)).fetchSet(TASK.ID);
+		List<CommitId> commitIdList = tasks.stream()
+			.filter(task -> task.getSource().isLeft())
+			.map(task -> task.getSource().getLeft().get())
+			.map(source -> new CommitId(source.getRepoId(), source.getHash()))
+			.collect(toList());
 
-		// Insert tasks that are not already in the queue
-		var insert = db.insertInto(TASK)
-			.columns(TASK.ID, TASK.AUTHOR, TASK.PRIORITY, TASK.INSERT_TIME, TASK.UPDATE_TIME,
-				TASK.TAR_NAME, TASK.REPO_ID, TASK.COMMIT_HASH);
+		Condition condition = DSL.noCondition();
 
-		for (Task task : tasks) {
-			if (existingIds.contains(task.getId().getId().toString())) {
-				continue; // task is already in table => skip!
+		for (CommitId commitId : commitIdList) {
+			Condition subCondition = TASK.REPO_ID.eq(commitId.getRepoId().getId().toString())
+				.and(TASK.COMMIT_HASH.eq(commitId.getHash().getHash()));
+
+			if (condition == null) {
+				condition = subCondition;
+			} else {
+				condition = condition.or(subCondition);
 			}
-
-			insert.values(
-				task.getId().getId().toString(),
-				task.getAuthor(),
-				task.getPriority(),
-				Timestamp.from(task.getInsertTime()),
-				Timestamp.from(task.getUpdateTime()),
-				task.getSource().getRight().map(TarSource::getTarName).orElse(null),
-				task.getSource()
-					.getLeft()
-					.map(RepoSource::getRepoId)
-					.map(RepoId::getId)
-					.map(UUID::toString)
-					.orElse(null),
-				task.getSource()
-					.getLeft()
-					.map(RepoSource::getHash)
-					.map(CommitHash::getHash)
-					.orElse(null)
-			);
 		}
 
-		insert.execute();
+		final Set<TaskId> inDatabaseTaskIds = new HashSet<>();
+		final Set<RepoSource> inDatabaseTaskCommitIds = new HashSet<>();
+		final Condition finalCondition = condition;
 
+		db.transaction(configuration -> {
+			DSLContext transaction = DSL.using(configuration);
+
+			transaction.selectFrom(TASK)
+				.where(TASK.ID.in(idList).or(finalCondition))
+				.fetch()
+				.forEach(taskRecord -> {
+					inDatabaseTaskIds.add(new TaskId(UUID.fromString(taskRecord.getId())));
+					if (taskRecord.getCommitHash() != null) {
+						inDatabaseTaskCommitIds.add(new RepoSource(
+							new RepoId(UUID.fromString(taskRecord.getRepoId())),
+							new CommitHash(taskRecord.getCommitHash())
+						));
+					}
+				});
+
+			// 2.) Remove all tasks from list that are already in database
+			tasks.removeIf(task -> inDatabaseTaskIds.contains(task.getId()));
+			tasks.removeIf(task -> task.getSource().isLeft()
+				&& inDatabaseTaskCommitIds.contains(task.getSource().getLeft().get()));
+
+			// 3.) Insert remaining tasks
+			if (!tasks.isEmpty()) {
+				var insert = transaction.insertInto(TASK)
+					.columns(TASK.ID, TASK.AUTHOR, TASK.PRIORITY, TASK.INSERT_TIME,
+						TASK.UPDATE_TIME, TASK.TAR_NAME, TASK.REPO_ID, TASK.COMMIT_HASH);
+
+				for (Task task : tasks) {
+					insert.values(
+						task.getId().getId().toString(),
+						task.getAuthor(),
+						task.getPriority(),
+						Timestamp.from(task.getInsertTime()),
+						Timestamp.from(task.getUpdateTime()),
+						task.getSource().getRight().map(TarSource::getTarName).orElse(null),
+						task.getSource()
+							.getLeft()
+							.map(RepoSource::getRepoId)
+							.map(RepoId::getId)
+							.map(UUID::toString)
+							.orElse(null),
+						task.getSource()
+							.getLeft()
+							.map(RepoSource::getHash)
+							.map(CommitHash::getHash)
+							.orElse(null)
+					);
+				}
+
+				insert.execute();
+			}
+		});
+
+		// 4.) Call listeners
 		for (Consumer<Task> insertHandler : insertHandlers) {
 			tasks.forEach(insertHandler);
 		}
 	}
 
 	/**
-	 * Delets all tasks whose ids match the given task ids.
+	 * Deletes all tasks whose ids match the given task ids.
 	 *
 	 * @param taskIds the ids of the tasks to delete
 	 */
