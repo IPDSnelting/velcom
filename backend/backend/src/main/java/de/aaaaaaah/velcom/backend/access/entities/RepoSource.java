029e733839e98bb7684646d138a57d0659effeb3
kwerber <werberkevin@gmail.com>
2020-07-23 20:27:29 +0200

    [backend] Implement equals() & hashCode() for RepoSource

--- backend/backend/src/main/java/de/aaaaaaah/velcom/backend/access/entities/RepoSource.java
+++ backend/backend/src/main/java/de/aaaaaaah/velcom/backend/access/entities/RepoSource.java
@@ -2,30 +2,51 @@ package de.aaaaaaah.velcom.backend.access.entities;
 
 import java.util.Objects;
 
+/**
+ * A source describing that the task originated from a commit in a repository.
+ */
 public class RepoSource {
 
-    private final RepoId repoId;
-    private final CommitHash hash;
+	private final RepoId repoId;
+	private final CommitHash hash;
 
-    public RepoSource(RepoId repoId, CommitHash hash) {
-        this.repoId = Objects.requireNonNull(repoId);
-        this.hash = Objects.requireNonNull(hash);
-    }
+	public RepoSource(RepoId repoId, CommitHash hash) {
+		this.repoId = Objects.requireNonNull(repoId);
+		this.hash = Objects.requireNonNull(hash);
+	}
 
-    public RepoId getRepoId() {
-        return repoId;
-    }
+	public RepoId getRepoId() {
+		return repoId;
+	}
 
-    public CommitHash getHash() {
-        return hash;
-    }
+	public CommitHash getHash() {
+		return hash;
+	}
 
-    @Override
-    public String toString() {
-        return "RepoSource{" +
-                "repoId=" + repoId +
-                ", hash=" + hash +
-                '}';
-    }
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		}
+		if (o == null || getClass() != o.getClass()) {
+			return false;
+		}
+		RepoSource source = (RepoSource) o;
+		return repoId.equals(source.repoId) &&
+			hash.equals(source.hash);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(repoId, hash);
+	}
+
+	@Override
+	public String toString() {
+		return "RepoSource{" +
+			"repoId=" + repoId +
+			", hash=" + hash +
+			'}';
+	}
 
 }
