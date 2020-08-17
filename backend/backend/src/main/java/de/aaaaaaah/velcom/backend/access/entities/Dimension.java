c4f9e74db67b2415857ed73821be15996dadbff6
kwerber <werberkevin@gmail.com>
2020-08-17 17:12:47 +0200

    [backend] Create Dimension entity

---
+++ backend/backend/src/main/java/de/aaaaaaah/velcom/backend/access/entities/Dimension.java
@@ -0,0 +1,56 @@
+package de.aaaaaaah.velcom.backend.access.entities;
+
+import java.util.Objects;
+
+public class Dimension {
+
+	private final MeasurementName name;
+	private final Unit unit;
+	private final Interpretation interpretation;
+
+	public Dimension(MeasurementName name, Unit unit,
+		Interpretation interpretation) {
+		this.name = Objects.requireNonNull(name);
+		this.unit = Objects.requireNonNull(unit);
+		this.interpretation = Objects.requireNonNull(interpretation);
+	}
+
+	public MeasurementName getName() {
+		return name;
+	}
+
+	public Unit getUnit() {
+		return unit;
+	}
+
+	public Interpretation getInterpretation() {
+		return interpretation;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		}
+		if (o == null || getClass() != o.getClass()) {
+			return false;
+		}
+		Dimension dimension = (Dimension) o;
+		return name.equals(dimension.name);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(name);
+	}
+
+	@Override
+	public String toString() {
+		return "Dimension{" +
+			"name=" + name +
+			", unit=" + unit +
+			", interpretation=" + interpretation +
+			'}';
+	}
+
+}
