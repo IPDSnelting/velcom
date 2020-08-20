f1af8c1e8f8df3a9670cbe39cfb41e7f5aca1df6
Joscha <joscha@plugh.de>
2020-08-20 16:53:50 +0000

    [backend] Delete BenchmarkStatus

--- backend/backend/src/main/java/de/aaaaaaah/velcom/backend/access/KnownCommitReadAccess.java
+++ backend/backend/src/main/java/de/aaaaaaah/velcom/backend/access/KnownCommitReadAccess.java
@@ -1,19 +1,11 @@
 package de.aaaaaaah.velcom.backend.access;
 
-import static de.aaaaaaah.velcom.backend.access.entities.BenchmarkStatus.BENCHMARK_REQUIRED;
-import static de.aaaaaaah.velcom.backend.access.entities.BenchmarkStatus.BENCHMARK_REQUIRED_MANUAL_PRIORITY;
 import static org.jooq.codegen.db.tables.KnownCommit.KNOWN_COMMIT;
 
-import de.aaaaaaah.velcom.backend.access.entities.BenchmarkStatus;
 import de.aaaaaaah.velcom.backend.access.entities.CommitHash;
 import de.aaaaaaah.velcom.backend.access.entities.RepoId;
 import de.aaaaaaah.velcom.backend.storage.db.DatabaseStorage;
-import de.aaaaaaah.velcom.backend.util.Pair;
-import java.util.Set;
-import java.util.UUID;
-import java.util.stream.Collectors;
 import org.jooq.DSLContext;
-import org.jooq.Record1;
 
 /**
  * Provides read access to known commits and their respective statuses.
