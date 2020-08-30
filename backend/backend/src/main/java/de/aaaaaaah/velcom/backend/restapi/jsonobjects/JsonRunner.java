15ba2c6899ac0fc3756c244a4f9c15209647c793
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2020-08-30 13:34:13 +0200

    [backend] Add JsonRunner and factory method to JsonTask

---
+++ backend/backend/src/main/java/de/aaaaaaah/velcom/backend/restapi/jsonobjects/JsonRunner.java
@@ -0,0 +1,53 @@
+package de.aaaaaaah.velcom.backend.restapi.jsonobjects;
+
+import de.aaaaaaah.velcom.backend.runner.KnownRunner;
+import java.time.Instant;
+import java.util.Objects;
+import java.util.UUID;
+import javax.annotation.Nullable;
+
+public class JsonRunner {
+
+	private final String name;
+	private final String info;
+	@Nullable
+	private final UUID workingOn;
+	@Nullable
+	private final Long workingSince;
+
+	public JsonRunner(String name, String info, @Nullable UUID workingOn,
+		@Nullable Long workingSince) {
+		this.name = Objects.requireNonNull(name, "name can not be null!");
+		this.info = Objects.requireNonNull(info, "info can not be null!");
+		this.workingOn = workingOn;
+		this.workingSince = workingSince;
+	}
+
+	public static JsonRunner fromKnownRunner(KnownRunner runner) {
+		return new JsonRunner(
+			runner.getName(),
+			runner.getInformation(),
+			runner.getCurrentTask().map(it -> it.getId().getId()).orElse(null),
+			Instant.now().getEpochSecond() // TODO: Implement
+		);
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+	public String getInfo() {
+		return info;
+	}
+
+	@Nullable
+	public UUID getWorkingOn() {
+		return workingOn;
+	}
+
+	@Nullable
+	public Long getWorkingSince() {
+		return workingSince;
+	}
+}
--- backend/backend/src/main/java/de/aaaaaaah/velcom/backend/restapi/jsonobjects/JsonTask.java
+++ backend/backend/src/main/java/de/aaaaaaah/velcom/backend/restapi/jsonobjects/JsonTask.java
@@ -1,5 +1,11 @@
 package de.aaaaaaah.velcom.backend.restapi.jsonobjects;
 
+import de.aaaaaaah.velcom.backend.access.CommitReadAccess;
+import de.aaaaaaah.velcom.backend.access.entities.Commit;
+import de.aaaaaaah.velcom.backend.access.entities.RepoId;
+import de.aaaaaaah.velcom.backend.access.entities.Task;
+import de.aaaaaaah.velcom.backend.access.entities.sources.CommitSource;
+import de.aaaaaaah.velcom.backend.access.entities.sources.TarSource;
 import java.util.UUID;
 
 public class JsonTask {
@@ -14,6 +20,34 @@ public class JsonTask {
 		this.author = author;
 		this.since = since;
 		this.source = source;
+	}
+
+	public static JsonTask fromTask(Task task, CommitReadAccess commitAccess) {
+		JsonSource source = task.getSource().consume(
+			commitSource -> getCommitSource(commitAccess, commitSource),
+			JsonTask::getTarSource
+		);
+
+		return new JsonTask(
+			task.getId().getId(), task.getAuthor(), task.getInsertTime().getEpochSecond(), source
+		);
+	}
+
+	private static JsonSource getTarSource(TarSource tarSource) {
+		return JsonSource.fromUploadedTar(tarSource.getDescription(),
+			tarSource.getRepoId().map(RepoId::getId).orElse(null));
+	}
+
+	private static JsonSource getCommitSource(CommitReadAccess commitAccess, CommitSource it) {
+		Commit commit = commitAccess.getCommit(it.getRepoId(), it.getHash());
+
+		return JsonSource.fromCommit(new JsonCommitDescription(
+			it.getRepoId().getId(),
+			it.getHash().getHash(),
+			commit.getAuthor(),
+			commit.getAuthorDate().getEpochSecond(),
+			commit.getSummary()
+		));
 	}
 
 	public UUID getId() {
