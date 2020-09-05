68149384fb1f327f0ea647724639d2d4304de18d
kwerber <werberkevin@gmail.com>
2020-09-05 16:04:51 +0200

    [backend] Add branches to RepoGraphData and remove unit and interpretation from it

--- backend/backend/src/main/java/de/aaaaaaah/velcom/backend/data/repocomparison/RepoGraphData.java
+++ backend/backend/src/main/java/de/aaaaaaah/velcom/backend/data/repocomparison/RepoGraphData.java
@@ -1,39 +1,46 @@
 package de.aaaaaaah.velcom.backend.data.repocomparison;
 
-import de.aaaaaaah.velcom.backend.access.entities.Interpretation;
-import de.aaaaaaah.velcom.backend.access.entities.Unit;
+import static java.util.Objects.requireNonNull;
+
+import de.aaaaaaah.velcom.backend.access.entities.BranchName;
 import de.aaaaaaah.velcom.backend.access.entities.RepoId;
 import java.util.List;
+import java.util.Set;
 
 public class RepoGraphData {
 
 	private final RepoId repoId;
+	private final Set<BranchName> includedBranches;
 	private final List<GraphEntry> entries;
-	private final Interpretation interpretation;
-	private final Unit unit;
 
-	public RepoGraphData(RepoId repoId,
-		List<GraphEntry> entries,
-		Interpretation interpretation, Unit unit) {
-		this.repoId = repoId;
-		this.entries = entries;
-		this.interpretation = interpretation;
-		this.unit = unit;
+	public RepoGraphData(RepoId repoId, Set<BranchName> includedBranches, List<GraphEntry> entries) {
+		this.repoId = requireNonNull(repoId);
+		this.includedBranches = requireNonNull(includedBranches);
+		this.entries = requireNonNull(entries);
 	}
 
 	public RepoId getRepoId() {
 		return repoId;
 	}
 
+	public Set<BranchName> getIncludedBranches() {
+		return includedBranches;
+	}
+
+	/**
+	 * @return a list containing all entries for this repository sorted by the author date of the
+	 * commits.
+	 */
 	public List<GraphEntry> getEntries() {
 		return entries;
 	}
 
-	public Interpretation getInterpretation() {
-		return interpretation;
+	@Override
+	public String toString() {
+		return "RepoGraphData{" +
+			"repoId=" + repoId +
+			", includedBranches=" + includedBranches +
+			'}';
 	}
 
-	public Unit getUnit() {
-		return unit;
-	}
 }
