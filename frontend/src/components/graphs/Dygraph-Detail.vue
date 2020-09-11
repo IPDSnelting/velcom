b611bc9d6114b7b95f04fb7a4419e91f3f8e5cd2
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2020-09-11 21:21:44 +0200

    [frontend] Migrate DytailGraph to new API before it is rewritten
    This is just a temporary solution until the graph is properly reworked.

--- frontend/src/components/graphs/Dygraph-Detail.vue
+++ frontend/src/components/graphs/Dygraph-Detail.vue
@@ -13,28 +13,14 @@ import Vue from 'vue'
 import Component from 'vue-class-component'
 import { Prop, Watch } from 'vue-property-decorator'
 import Dygraph from 'dygraphs'
-import {
-  Commit,
-  CommitComparison,
-  Datapoint,
-  Measurement,
-  MeasurementID
-} from '../../store/types'
-import { crosshairIcon } from '../graphs/crosshairIcon'
-import { vxm } from '../../store'
-import { formatDateUTC } from '../../util/TimeUtil'
+import { DetailDataPoint, Dimension, DimensionId } from '@/store/types'
+import { vxm } from '@/store'
 import 'dygraphs/dist/dygraph.css'
-
-type CommitInfo = {
-  commit: Commit
-  comparison: CommitComparison
-  measurementId: MeasurementID
-}
 
 @Component({})
 export default class DytailGraph extends Vue {
-  @Prop({})
-  measurements!: MeasurementID[]
+  @Prop()
+  dimensions!: Dimension[]
 
   @Prop({ default: true })
   beginYAtZero!: boolean
@@ -43,138 +29,61 @@ export default class DytailGraph extends Vue {
 
   private height: number = 500
 
-  private get datapoints(): CommitInfo[] {
-    return vxm.detailGraphModule.repoHistory
-      .slice()
-      .reverse()
-      .flatMap(it => {
-        return this.measurements.map(measurementId => {
-          return {
-            commit: it.commit,
-            comparison: it.comparison,
-            measurementId: measurementId
-          }
-        })
-      })
+  private get datapoints(): DetailDataPoint[] {
+    return vxm.detailGraphModule.detailGraph
   }
 
-  // https://stackoverflow.com/questions/14446511/most-efficient-method-to-groupby-on-an-array-of-objects
-  private groupBy<K, V>(list: K[], keyGetter: (key: K) => V) {
-    const map = new Map()
-    list.forEach(item => {
-      const key = keyGetter(item)
-      const collection = map.get(key)
-      if (!collection) {
-        map.set(key, [item])
-      } else {
-        collection.push(item)
-      }
-    })
-    return map
-  }
-  private get groupedByMeasurement(): Map<string, CommitInfo[]> {
-    return this.groupBy(this.datapoints, it => it.measurementId.toString())
-  }
-
-  // prettier-ignore
-  private get wantedMeasurementForDatapoint(): (comparison: CommitComparison, measurementId: MeasurementID) => Measurement | undefined {
-    return (comparison: CommitComparison, measurementId: MeasurementID) => {
-      if (
-        comparison.second &&
-        comparison.second.measurements
-      ) {
-        let wantedMeasurement: Measurement | undefined =
-          comparison.second.measurements.find(it => it.id.equals(measurementId))
-        return wantedMeasurement
-      }
-      return undefined
-    }
-  }
-
-  private datapointValue(datapoint: {
-    commit: Commit
-    comparison: CommitComparison
-    measurementId: MeasurementID
-  }): number | null {
-    let wantedMeasurement = this.wantedMeasurementForDatapoint(
-      datapoint.comparison,
-      datapoint.measurementId
-    )
-    if (wantedMeasurement !== undefined && wantedMeasurement.value !== null) {
-      return wantedMeasurement.value
-    }
-    return null
-  }
-
-  // retrieving and interpreting datapoints
-  private get amount(): number {
-    return Number.parseInt(vxm.detailGraphModule.selectedFetchAmount)
-  }
-
-  private unit(measurementId: MeasurementID): string | null {
+  private firstSuccessful(dimension: DimensionId): number {
     for (const datapoint of this.datapoints) {
-      let wantedMeasurement = this.wantedMeasurementForDatapoint(
-        datapoint.comparison,
-        measurementId
-      )
-      if (wantedMeasurement !== undefined && wantedMeasurement.unit) {
-        return wantedMeasurement.unit
-      }
-    }
-    return null
-  }
-
-  private firstSuccessful(measurementId: MeasurementID): number {
-    for (const datapoint of this.datapoints) {
-      let wantedMeasurement = this.wantedMeasurementForDatapoint(
-        datapoint.comparison,
-        measurementId
-      )
-      if (
-        wantedMeasurement !== undefined &&
-        wantedMeasurement.successful &&
-        wantedMeasurement.value
-      ) {
-        return wantedMeasurement.value
+      const value = datapoint.values.get(dimension)
+      if (typeof value === 'number') {
+        return value
       }
     }
     return this.height / 2
   }
 
   private get yLabel(): string {
-    if (this.measurements.length === 1) {
-      return this.unit(this.measurements[0])
-        ? this.measurements[0].metric + ' in ' + this.unit(this.measurements[0])
-        : this.measurements[0].metric
+    if (this.dimensions.length === 1) {
+      return this.dimensions[0].metric + ' in ' + this.dimensions[0].unit
     }
     return ''
   }
 
   @Watch('datapoints')
   private up() {
-    let data: any[] = []
-    for (let i = 0; i < this.amount; i++) {
-      data.push([i])
+    const data: number[][] = []
+
+    // One array entry per datapoint. That array contains all values: [x, dim1, dim2, ...]
+    for (let i = 0; i < this.datapoints.length; i++) {
+      data[i] = [this.datapoints[i].authorDate.getTime()]
     }
-    for (let [n, info] of this.groupedByMeasurement.entries()) {
-      let lastValue = this.firstSuccessful(info[0].measurementId)
-      info.forEach((p, index) => {
-        let value =
-          this.datapointValue(p) === null ? lastValue : this.datapointValue(p)
-        lastValue = value!
-        data[index].push(value)
+
+    for (const dimension of this.dimensions) {
+      let lastValue = this.firstSuccessful(dimension)
+      this.datapoints.forEach((point, index) => {
+        let pointValue = point.values.get(dimension)
+        if (typeof pointValue !== 'number') {
+          pointValue = lastValue
+        }
+        lastValue = pointValue
+
+        data[index].push(pointValue)
       })
     }
 
     this.g.updateOptions({
       file: data,
-      labels: ['x', ...Array.from(this.groupedByMeasurement.keys())]
+      labels: [
+        'x',
+        ...this.dimensions.map(it => it.benchmark + ' - ' + it.metric)
+      ]
     })
   }
 
-  private mounted() {
+  mounted(): void {
     // empty string because div is not expected to be NULL
-    let chartDiv = document.getElementById('chart') || ''
+    const chartDiv = document.getElementById('chart') || ''
 
     this.g = new Dygraph(
       // containing div
