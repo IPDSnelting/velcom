4f0b48d041cab7c48dbde20bacdfcf1f32442226
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2020-10-31 17:01:14 +0100

    [frontend] Fix home page test

--- frontend/cypress/integration/home/home.spec.ts
+++ frontend/cypress/integration/home/home.spec.ts
@@ -8,16 +8,12 @@ function loadMoreRuns(countAfter: number, alias: string) {
   const significantOnly = alias.indexOf('significant') > 0
 
   if (significantOnly) {
-    cy.route(`/api/recent/runs?n=${countAfter}&significant=true`).as(
-      'fetch_more'
-    )
+    cy.route(`/recent/runs?n=${countAfter}&significant=true`).as('fetch_more')
   } else {
-    cy.route(`/api/recent/runs?n=${countAfter}`).as('fetch_more')
+    cy.route(`/recent/runs?n=${countAfter}`).as('fetch_more')
   }
 
-  cy.get(alias)
-    .contains('load more')
-    .click()
+  cy.get(alias).contains('load more').click()
 
   cy.wait(['@fetch_more'])
 }
@@ -31,11 +27,6 @@ context('Homepage', () => {
   })
 
   it('displays recently benchmarked runs', () => {
-    cy.route({
-      method: 'GET',
-      url: '/api/recent/runs?n=10&significant=true',
-      response: 'fixture:homepage_run_example.json'
-    }).as('significantDefaultRoute')
     cy.visit('home')
 
     cy.contains('Recent Significant Runs').should('exist')
@@ -47,7 +38,7 @@ context('Homepage', () => {
     cy.get('@run_overview').contains('VelCom')
 
     cy.get('@run_overview').contains(
-      '[backend] Find and return significant recent runs'
+      '[frontend] Disable "Deselect all metrics" when no metrics are selected'
     )
 
     cy.get('@run_overview').contains('Joscha <joscha@plugh.de>')
@@ -55,7 +46,7 @@ context('Homepage', () => {
     cy.get('@run_overview').contains('backend')
     cy.get('@run_overview').contains('coverage')
 
-    cy.get('@run_overview').contains('ccbfc79b611cb4132e0c097ac9d412f6ca6a1b31')
+    cy.get('@run_overview').contains('deec6c90541a9064849af7011fa9d7c6f40a3de1')
   })
 
   it('fetches more significant runs', () => {
@@ -67,18 +58,14 @@ context('Homepage', () => {
     countRuns('@recent_significant').should('have.length', 15)
 
     // The pagination should be hidden for < 20 runs
-    cy.get('@recent_significant')
-      .contains('Items per page')
-      .should('not.exist')
+    cy.get('@recent_significant').contains('Items per page').should('not.exist')
 
     loadMoreRuns(20, '@recent_significant')
 
     countRuns('@recent_significant').should('have.length', 20)
 
     // The pagination should be shown now
-    cy.get('@recent_significant')
-      .contains('Items per page')
-      .should('exist')
+    cy.get('@recent_significant').contains('Items per page').should('exist')
   })
 
   it('fetches more normal runs', () => {
@@ -90,9 +77,7 @@ context('Homepage', () => {
     countRuns('@recent_runs').should('have.length', 15)
 
     // The pagination should be hidden for < 20 runs
-    cy.get('@recent_runs')
-      .contains('Items per page')
-      .should('not.exist')
+    cy.get('@recent_runs').contains('Items per page').should('not.exist')
 
     // 25 loaded
     loadMoreRuns(25, '@recent_runs')
@@ -101,9 +86,7 @@ context('Homepage', () => {
     countRuns('@recent_runs').should('have.length', 20)
 
     // The pagination should be shown now
-    cy.get('@recent_runs')
-      .contains('Items per page')
-      .should('exist')
+    cy.get('@recent_runs').contains('Items per page').should('exist')
   })
 
   it('should take you to the detail page when clicking the message', () => {
@@ -114,17 +97,15 @@ context('Homepage', () => {
       .as('run_overview')
 
     cy.get('@run_overview')
-      .contains('[backend] Find and return significant recent runs')
+      .contains(
+        '[frontend] Disable "Deselect all metrics" when no metrics are selected'
+      )
       .click()
 
     cy.location('pathname').should('include', 'run-detail')
     cy.location('pathname').should(
       'include',
-      '44bb5c8d-b20d-4bef-bdad-c92767dfa489'
-    )
-    cy.location('pathname').should(
-      'include',
-      'ccbfc79b611cb4132e0c097ac9d412f6ca6a1b31'
+      '59bb6ef0-6f45-42ab-8fbc-db17ffab6679'
     )
 
     cy.go('back')
@@ -138,9 +119,7 @@ context('Homepage', () => {
       .find('[data-cy="run_overview"]')
       .as('run_overview')
 
-    cy.get('@run_overview')
-      .contains('VelCom')
-      .click()
+    cy.get('@run_overview').contains('VelCom').click()
 
     cy.location('pathname').should('include', 'repo-detail')
     cy.location('pathname').should(
