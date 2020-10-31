a94eff9d8ad74cd81f9ed45ee763c25613839a4b
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2020-10-31 17:00:56 +0100

    [meta] Add way to start mock backend and frontend together

---
+++ .integration-test/start_backend_and_frontend.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# This is a simple script to start two programs (backend and frontend) and
+# relay signals to both.
+
+echo "Starting backend"
+make run-backend &
+# Save its PID so we can relay kill signals
+BACKEND_PID="$!"
+
+
+echo "Starting frontend"
+(cd ../frontend && yarn serve --mode=development) &
+# Save its PID so we can relay kill signals
+FRONTEND_PID="$!"
+
+# The shell does not pass on exit signals, so we do it manually
+trap "kill $FRONTEND_PID && kill $BACKEND_PID" exit INT TERM
+
+# Wait for the subprocesses (= spawned by &) to finish
+wait
