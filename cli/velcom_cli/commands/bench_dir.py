518195d923834d499be6a20689fc5a72d96334ee
Joscha <joscha@plugh.de>
2021-01-17 17:09:16 +0100

    [cli] Specify repo for task via --repo or -r

--- cli/velcom_cli/commands/bench_dir.py
+++ cli/velcom_cli/commands/bench_dir.py
@@ -17,18 +17,60 @@ def register(subparsers):
         nargs="?",
         type=Path,
         metavar="BENCHDIR",
-        help="directory to benchmark (default: current directory)"
+        help="directory to benchmark (default: current directory)",
+    )
+    parser.add_argument(
+        "--repo", "-r",
+        metavar="REPO",
+        help="id or name of the repo this task should be attached to",
     )
     parser.set_defaults(f=command)
 
 
-# TODO Allow specifying a repo
+def normalize(s):
+    """
+    Remove all of a string's whitespace characters and lowercase it.
+    """
+
+    return "".join(c for c in s.lower() if not c.isspace())
+
+
+def find_repo_id(repos, repo):
+    # Search by ID
+    ids = {repo["id"] for repo in repos}
+    if repo in ids:
+        return repo
+
+    # Strict name search
+    ids_by_name = {repo["name"]: repo["id"] for repo in repos}
+    if repo in ids_by_name:
+        return ids_by_name[repo]
+
+    # More lenient name search
+    ids_by_name = {normalize(repo["name"]): repo["id"] for repo in repos}
+    if repo in ids_by_name:
+        return ids_by_name[normalize(repo)]
+
+    print(f"Invalid repo id or name: {repo!r}")
+    print("Available repos:")
+    for repo in repos:
+        print(f"{repo['id']} - {repo['name']}")
+    exit(1)
+
+
 def command(config):
     site_url = config.geturl("site_url")
     api_url = config.geturl("api_url")
     admin_pw = config.get("admin_pw")
 
     bench_dir = config.args.bench_dir or Path.cwd()
+
+    # Find repo id
+    repo_id = None
+    if repo := config.args.repo:
+        response = requests.get(api_url + "all-repos")
+        repos = json.loads(response.text)["repos"]
+        repo_id = find_repo_id(repos, repo)
 
     print("Creating temporary file")
     with tempfile.TemporaryFile() as tmpf:
@@ -45,7 +87,7 @@ def command(config):
         response = requests.post(
             upload_api_url,
             auth=("admin", admin_pw),
-            data={"description": description},
+            data={"description": description, "repo_id": repo_id},
             # Specifying a file name ending in ".tar.gz" so the server knows
             # that this is a gzipped tar file
             files={"file": ("upload.tar.gz", tmpf)},
