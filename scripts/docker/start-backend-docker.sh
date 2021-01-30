698666abf8a020e47c1953949b083815f8b70e50
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2021-01-30 17:39:53 +0100

    [meta] Add docker build and run scripts

---
+++ scripts/docker/start-backend-docker.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+# This is a sample docker entrypoint. It is used by the provided docker image
+# to start up the backend.
+
+# Relevant parts:
+#  1. Start nginx AND our application
+#  2. Relay SIGTERM and SIGINT to our java application so the docker container
+#     can be stopped properly
+
+# Start nginx for routing
+nginx &
+
+cd "/home/velcom"
+
+echo "Running in $PWD"
+
+# Start our backend
+if [ -f /home/velcom/aspectjweaver.jar ]; then
+    sudo -Eu velcom $(which java) -javaagent:"/home/velcom/aspectjweaver.jar" -jar /home/velcom/velcom.jar server "$1" &
+else
+    sudo -Eu velcom $(which java) -jar /home/velcom/velcom.jar server "$1" &
+fi
+
+# Save its PID so we can relay kill signals
+JAVA_PID="$!"
+
+# The shell does not pass on exit signals, so we do it manually
+# This ensures java is killed properly and the container can restart.
+trap "kill $JAVA_PID" exit INT TERM
+
+# Wait for the subprocesses (= spawned by &) to finish
+wait
