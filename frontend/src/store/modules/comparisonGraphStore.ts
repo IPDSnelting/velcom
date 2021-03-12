76fac26f0bf15a91155b27cad046475c23a5dc90
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2021-03-12 17:42:30 +0100

    [frontend] Add RepoBranchSelector for comparison graph

--- frontend/src/store/modules/comparisonGraphStore.ts
+++ frontend/src/store/modules/comparisonGraphStore.ts
@@ -1,8 +1,37 @@
-import { createModule } from 'vuex-class-component'
+import { createModule, mutation } from 'vuex-class-component'
+import { RepoId } from '@/store/types'
+import Vue from 'vue'
 
 const VxModule = createModule({
   namespaced: 'comparisonGraphModule',
   strict: false
 })
 
-export class ComparisonGraphStore extends VxModule {}
+export class ComparisonGraphStore extends VxModule {
+  private _selectedBranches: { [id: string]: string[] } = {}
+
+  @mutation
+  setSelectedBranchesForRepo(payload: {
+    repoId: RepoId
+    branches: string[]
+  }): void {
+    Vue.set(this._selectedBranches, payload.repoId, payload.branches)
+  }
+
+  @mutation
+  toggleRepoBranch(payload: { repoId: RepoId; branch: string }): void {
+    let branches: string[] = this._selectedBranches[payload.repoId] || []
+
+    if (branches.includes(payload.branch)) {
+      branches = branches.filter(it => it !== payload.branch)
+    } else {
+      branches.push(payload.branch)
+    }
+
+    Vue.set(this._selectedBranches, payload.repoId, branches)
+  }
+
+  get selectedBranchesForRepo(): (repoId: RepoId) => string[] {
+    return repoId => this._selectedBranches[repoId] || []
+  }
+}
