8c555db198a9ab62465095013beb49b9f60abe45
Joscha <joscha@plugh.de>
2021-03-31 20:09:28 +0200

    [backend] Understand GitHub URLs

--- backend/backend/src/main/java/de/aaaaaaah/velcom/backend/access/repoaccess/entities/Repo.java
+++ backend/backend/src/main/java/de/aaaaaaah/velcom/backend/access/repoaccess/entities/Repo.java
@@ -4,6 +4,8 @@ import java.time.Instant;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import javax.annotation.Nullable;
 
 /**
@@ -15,18 +17,23 @@ public class Repo {
 	private final String name;
 	private final RemoteUrl remoteUrl;
 	@Nullable
-	private final String ghAccessToken;
+	private final String githubAccessToken;
 	@Nullable
-	private final Instant ghActivationTime;
+	private final Instant githubActivationTime;
 
-	public Repo(RepoId id, String name, RemoteUrl remoteUrl, @Nullable String ghAccessToken,
-		@Nullable Instant ghActivationTime) {
+	public Repo(RepoId id, String name, RemoteUrl remoteUrl, @Nullable String githubAccessToken,
+		@Nullable Instant githubActivationTime) {
+
+		if ((githubAccessToken == null) != (githubActivationTime == null)) {
+			throw new IllegalArgumentException(
+				"ghAccessToken and ghActivationTime must either both be present or both be null");
+		}
 
 		this.id = id;
 		this.name = name;
 		this.remoteUrl = remoteUrl;
-		this.ghAccessToken = ghAccessToken;
-		this.ghActivationTime = ghActivationTime;
+		this.githubAccessToken = githubAccessToken;
+		this.githubActivationTime = githubActivationTime;
 	}
 
 	public RepoId getId() {
@@ -53,12 +60,24 @@ public class Repo {
 		return remoteUrl.getUrl();
 	}
 
-	public Optional<String> getGhAccessToken() {
-		return Optional.ofNullable(ghAccessToken);
+	public Optional<String> getGithubRepoName() {
+		String remoteUrl = getRemoteUrlAsString();
+		Pattern pattern = Pattern.compile("github.com[:/]([^/]+/[^/.]+)(\\.git)?");
+		Matcher matcher = pattern.matcher(remoteUrl);
+		if (matcher.find()) {
+			return Optional.of(matcher.group(1));
+		} else {
+			return Optional.empty();
+		}
 	}
 
-	public Optional<Instant> getGhActivationTime() {
-		return Optional.ofNullable(ghActivationTime);
+	public Optional<GithubInfo> getGithubInfo() {
+		Optional<String> repoName = getGithubRepoName();
+		if (repoName.isPresent() && githubAccessToken != null && githubActivationTime != null) {
+			return Optional.of(new GithubInfo(repoName.get(), githubAccessToken, githubActivationTime));
+		} else {
+			return Optional.empty();
+		}
 	}
 
 	@Override
@@ -84,7 +103,63 @@ public class Repo {
 			"id=" + id.getIdAsString() +
 			", name='" + name + '\'' +
 			", remoteUrl='" + remoteUrl.getUrl() + '\'' +
-			", ghActivationTime=" + ghActivationTime +
+			", githubActivationTime=" + githubActivationTime +
 			'}';
+	}
+
+	public static class GithubInfo {
+
+		private final String repoName;
+		private final String accessToken;
+		private final Instant activationTime;
+
+		public GithubInfo(String repoName, String accessToken, Instant activationTime) {
+			this.repoName = repoName;
+			this.accessToken = accessToken;
+			this.activationTime = activationTime;
+		}
+
+		public String getRepoName() {
+			return repoName;
+		}
+
+		public String getAccessToken() {
+			return accessToken;
+		}
+
+		/**
+		 * @return the time when the github access token was set. Pull requests and commands from before
+		 * 	this time should be ignored.
+		 */
+		public Instant getActivationTime() {
+			return activationTime;
+		}
+
+		@Override
+		public boolean equals(Object o) {
+			if (this == o) {
+				return true;
+			}
+			if (o == null || getClass() != o.getClass()) {
+				return false;
+			}
+			GithubInfo that = (GithubInfo) o;
+			return Objects.equals(repoName, that.repoName) && Objects
+				.equals(accessToken, that.accessToken) && Objects
+				.equals(activationTime, that.activationTime);
+		}
+
+		@Override
+		public int hashCode() {
+			return Objects.hash(repoName, accessToken, activationTime);
+		}
+
+		@Override
+		public String toString() {
+			return "GithubInfo{" +
+				"repoName='" + repoName + '\'' +
+				", activationTime=" + activationTime +
+				'}';
+		}
 	}
 }
